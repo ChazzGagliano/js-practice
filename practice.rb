a6202d091fdf2ccdd67ab17b8b92c8cb09133f76
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-09-21 18:31:08 -0400

    less than 100 ruby practice

--- practice.rb
+++ practice.rb
@@ -1,14 +1,17 @@
-# Write a function that returns the sum of all numbers in a given array.
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
 
-def sum_array(array)
-    sum = 0
 
+def less_than(array)
+    new_array = []
     array.each do |number|
-        sum += number
+        if number < 100
+            new_array << number
+        end
     end
-    return sum
+
+    return new_array
+
 end
 
-array = [1, 2, 3, 4]
-
-p sum_array(array)
+array = [99, 101, 88, 4, 2000, 50]
+p less_than(array)
