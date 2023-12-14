b6c0e42f5beefbd45d87cc3a5ea9d7b89d6fc8f9
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-12-14 00:40:49 -0500

    repeat

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
 function toStrings(array) {
-    let i = 0
     let new_array = []
-    while( i < array.length) {
+    let i = 0
+    while(i < array.length) {
         array[i] = array[i].toString()
         new_array.push(array[i])
         i ++
