fa5277a8beebde2a442078603a4a560f38f8a90b
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-12-15 17:09:16 -0500

    We're talking about practice

--- practice.js
+++ practice.js
@@ -1,13 +1,21 @@
-function toStrings(array) {
-    let new_array = []
-    let i = 0
-    while(i < array.length) {
-        array[i] = array[i].toString()
-        new_array.push(array[i])
-        i ++
+function max_pair(array) {
+    sum = []
+    for (let i = 0; i < array.length; i++ ) {
+        for (let i2 = 0; i2 < array.length; i2 ++ ) {
+            sum.push(array[i] - array[i2])
+        }
     }
-    return new_array
+    max = sum[0]
+    index = 0
+    while (index < sum.length) {
+        if (max < sum[index]) {
+            max = sum[index]
+        }
+        index ++
+    }
+    return max
 }
 
-array = [1, 2, 3, 4]
-console.log(toStrings(array))
+array = [1, 4, 20, 3, 9, 8]
+console.log(max_pair(array))
+console.log(sum)
